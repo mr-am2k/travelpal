0f6ef483e55c3e7f97681a1acc5e75f9de8b0dad
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-10-15 15:02:48 +0200

    Updated User schema

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,8 @@
 const User = require("../models/User")
 const { StatusCodes } = require('http-status-codes')
 const registerUser = async (req, res) => {  
-    const user = await User.create(req.body)
+    const sentUser = req.body
+    const user = await User.create(sentUser)
     const { ...returnObject } = user._doc
     delete returnObject.passwordHash
     const token = user.createJWT();
--- backend/routes/postsRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const getPosts = require('../controllers/postsController')
-
-router.get('/', getPosts)
-
-
-module.exports = router
