4ac28199e91aec7a004e134f81f6d24d6da99e57
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-10-15 19:03:04 +0200

    head fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,11 @@ import "aos/dist/aos.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
-<<<<<<< HEAD
-import UserFeed from "./pages/UserFeed";
-=======
+
 export const Context = createContext({
   loggedIn: false,
   setLoggedIn: (loggedIn) => {},
 });
->>>>>>> ea1e64c045e5a2d69eeecb06a9f0945f674524a8
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   useEffect(() => {
@@ -21,15 +18,6 @@ function App() {
     AOS.refresh();
   }, []);
   return (
-<<<<<<< HEAD
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<p>ABOUT</p>} />
-      <Route path="login" element={<Login />} />
-      <Route path="registration" element={<Registration />} />
-      <Route path="userfeed" element={<UserFeed />} />
-    </Routes>
-=======
     <Context.Provider value={{ loggedIn, setLoggedIn }}>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -38,7 +26,6 @@ function App() {
         <Route path="registration" element={<Registration />} />
       </Routes>
     </Context.Provider>
->>>>>>> ea1e64c045e5a2d69eeecb06a9f0945f674524a8
   );
 }
 
