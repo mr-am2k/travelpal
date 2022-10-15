a1176d7603f1f9346af443e2e6c59aeb7c6cc07b
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-10-15 10:56:48 +0200

    feat: aos initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Home from "./pages/Home";
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<p>HOME </p>} />
+      <Route path="/" element={<Home />} />
       <Route path="about" element={<p>ABOUT</p>} />
     </Routes>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div data-aos="fade-up" className="h-20 w-20 bg-black">
+      HOME
+    </div>
+  );
 };
 
 export default Home;
