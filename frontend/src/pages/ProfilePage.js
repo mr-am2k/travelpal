51a0ae578822ae00d5031f9db9362688e728ca65
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2022-10-16 12:53:40 +0200

    changes

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,5 +1,12 @@
 import profilePic1 from "../images/Feed/feed1.png";
 import Navbar from "../components/Navbar";
+import Stars from "../images/Stars.png";
+import UserProfileIcons from "../components/UserProfileIcons";
+
+import place1 from "../images/placeIcons/place1.png";
+import place2 from "../images/placeIcons/place2.png";
+import place3 from "../images/placeIcons/place3.png";
+import place4 from "../images/placeIcons/place4.png";
 const ProfilePage = (props) => {
   return (
     <>
@@ -18,7 +25,48 @@ const ProfilePage = (props) => {
         </div>
         <div className="w-[60%] flex justify-center mt-[100px]">
           <div className="w-[70%]">
-            <h2 className="font-bold">About</h2>
+            <h2 className="font-bold text-[20px] border-b-2">About</h2>
+            <div className="w-[100%] flex flex-wrap">
+              <div className="w-[50%] h-[100px] py-[10px] text-[14px]">
+                <p>
+                  <span className="font-bold">Name:</span> Zinna Youanni
+                </p>
+                <p>
+                  <span className="font-bold">Username:</span> @zinna
+                </p>
+                <p>
+                  <span className="font-bold">From:</span> Algeria
+                </p>
+              </div>
+              <div className="w-[50%] h-[100px] py-[10px] text-[14px]">
+                <p>
+                  <span className="font-bold">Age:</span> 21
+                </p>
+                <p>
+                  <span className="font-bold">Rating:</span>
+                </p>
+                <img src={Stars} width="100px" />
+              </div>
+              <div className="flex flex-col w-[50%] h-[100px] py-[10px] text-[14px]">
+                <h2 className="text-[18px] font-bold">Languages</h2>
+                <p>&#x2022; English</p>
+                <p>&#x2022; French</p>
+                <p>&#x2022; Turkish</p>
+              </div>
+              <div className="flex flex-col w-[50%] h-[100px] py-[10px] text-[14px]">
+                <h2 className="text-[18px] font-bold">Currently in</h2>
+                <p>Algeria</p>
+              </div>
+              <div className="w-[100%] mt-[30px]">
+                <h2 className="text-[16px] font-bold">Suggested places</h2>
+                <div className="flex">
+                  <UserProfileIcons image={place1} name="Sarajevo" country="" />
+                  <UserProfileIcons image={place2} name="Sarajevo" country="" />
+                  <UserProfileIcons image={place3} name="Sarajevo" country="" />
+                  <UserProfileIcons image={place4} name="Sarajevo" country="" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
