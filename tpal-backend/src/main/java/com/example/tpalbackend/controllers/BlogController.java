0b7e692cbf96b34e5865912575ee2290554205eb
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-12-28 21:14:00 +0100

    feat: get all blogs added, no pagination, filtering

--- tpal-backend/src/main/java/com/example/tpalbackend/controllers/BlogController.java
+++ tpal-backend/src/main/java/com/example/tpalbackend/controllers/BlogController.java
@@ -1,6 +1,4 @@
 package com.example.tpalbackend.controllers;
-
-import com.example.tpalbackend.entities.BlogEntity;
 import com.example.tpalbackend.payload.request.BlogCreateRequest;
 import com.example.tpalbackend.payload.response.GlobalResponse;
 import com.example.tpalbackend.services.blog.BlogService;
@@ -8,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.net.URI;
 import java.util.Optional;
 
@@ -17,6 +14,19 @@ import java.util.Optional;
 @RequestMapping("/api/v1/blog")
 public class BlogController {
     private final BlogService _blogService;
+    @GetMapping
+    public ResponseEntity<GlobalResponse> GetAll(){
+        var response = new GlobalResponse();
+        try{
+            response.setData(Optional.ofNullable(this._blogService.getAll()));
+            return ResponseEntity.ok().body(response);
+        }catch(Exception ex){
+            response.setSuccess(false);
+            response.setMessage(ex.getMessage().describeConstable());
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
     @PostMapping
     public ResponseEntity<GlobalResponse> Post(@RequestBody BlogCreateRequest blog){
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/blogs").toUriString());
