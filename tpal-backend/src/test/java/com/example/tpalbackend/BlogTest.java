136ccf7eed874045c8cdd56faf457d5eca6531ba
MahirPrcanovic <mahirprcanovic7@gmail.com>
2023-03-27 13:59:36 +0200

    feat: blog test

--- tpal-backend/src/test/java/com/example/tpalbackend/BlogTest.java
+++ tpal-backend/src/test/java/com/example/tpalbackend/BlogTest.java
@@ -1,6 +1,7 @@
 package com.example.tpalbackend;
 
 import com.example.tpalbackend.entities.BlogEntity;
+import com.example.tpalbackend.entities.UserEntity;
 import com.example.tpalbackend.middleware.exceptions.UserNotFoundByIdException;
 import com.example.tpalbackend.payload.request.blog.BlogCreateRequest;
 import com.example.tpalbackend.repositories.blog.BlogJpaRepository;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.UUID;
@@ -32,32 +35,39 @@ public class BlogTest {
         blogRepository = Mockito.mock(BlogJpaRepository.class);
         userRepository = Mockito.mock(UserJpaRepository.class);
         blogService = new DefaultBlogService(blogRepository,userRepository);
-    }
 
-    @DisplayName("Blog creation")
+    }
+    void mockAuthentication(){
+        Authentication authentication = Mockito.mock(Authentication.class);
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        UserEntity entity = Mockito.mock(UserEntity.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        Mockito.when(securityContext.getAuthentication().getName()).thenReturn("mahir");
+        Mockito.when(userRepository.findByUsername("mahir")).thenReturn(entity);
+        SecurityContextHolder.setContext(securityContext);
+    }
+    @DisplayName("Blog creation success test")
     @Test
     void createNewBlog(){
         BlogCreateRequest newRequest = new BlogCreateRequest();
         newRequest.setTitle("new title!");
         newRequest.setDescription("new description!");
         var createdBlog = new BlogEntity(UUID.randomUUID(),"new title!","new description!",null,null);
+        mockAuthentication();
         Mockito.when(blogService.create(newRequest)).thenReturn(createdBlog);
-
         var createdBlogResponse = blogService.create(newRequest);
         Assertions.assertThat(createdBlogResponse).as("Check all fields")
                 .usingRecursiveComparison().ignoringFields("user","comments","id").isEqualTo(createdBlog);
     }
-    @DisplayName("Blog creation exception throw")
+    @DisplayName("Blog user not found exception test")
     @Test
     void createBlogFail(){
         BlogCreateRequest newRequest = new BlogCreateRequest();
         newRequest.setTitle("new title!");
         newRequest.setDescription("new description!");
         var createdBlog = new BlogEntity("new title!","new description!",null);
-        Mockito.when(blogRepository.save(createdBlog)).thenReturn(null);
-        Mockito.when(userRepository.findByUsername(null)).thenReturn(null);
-        Mockito.when(SecurityContextHolder.getContext().getAuthentication()).thenReturn(null);
-        UserNotFoundByIdException exception = assertThrows(UserNotFoundByIdException.class,()->blogService.create(newRequest));
-        assertEquals(exception.getMessage(),"User not found.");
+        assertThrows(UserNotFoundByIdException.class, () -> {
+            blogService.create(newRequest);
+        });
     }
 }
--- tpal-backend/src/test/java/com/example/tpalbackend/PostTest.java
+++ tpal-backend/src/test/java/com/example/tpalbackend/PostTest.java
@@ -1,7 +1,29 @@
 package com.example.tpalbackend;
 
+import com.example.tpalbackend.repositories.blog.BlogJpaRepository;
+import com.example.tpalbackend.repositories.post.PostJpaRepository;
+import com.example.tpalbackend.repositories.user.UserJpaRepository;
+import com.example.tpalbackend.services.blog.BlogService;
+import com.example.tpalbackend.services.blog.DefaultBlogService;
+import com.example.tpalbackend.services.post.DefaultPostService;
+import com.example.tpalbackend.services.post.PostService;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PostTest {
+    @Autowired
+
+    static PostService postService;
+    static UserJpaRepository userRepository;
+    static PostJpaRepository postRepository;
+    @BeforeAll
+    static void beforeMethod(){
+        postRepository = Mockito.mock(PostJpaRepository.class);
+        userRepository = Mockito.mock(UserJpaRepository.class);
+        postService = new DefaultPostService(postRepository,userRepository);
+    }
+
 }
