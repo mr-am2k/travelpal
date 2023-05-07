debc11bac55e7a1deecc722313bc04dec46be8ea
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2023-05-07 11:02:34 +0200

    profile page made responsive (needs refactoring Spaghetti code)

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -12,8 +12,8 @@ const ProfilePage = (props) => {
   return (
     <>
       <Navbar />
-      <div className="flex">
-        <div className="w-[40%] h-[500px] flex flex-col items-center justify-around mt-[50px] border-r-2">
+      <div className="flex flex-col items-center md:flex-row">
+        <div className="w-[100%] md:w-[40%] h-[500px] flex flex-col items-center justify-around md:mt-[50px] border-r-2">
           <div className="flex flex-col items-center">
             <div
               style={{
@@ -22,25 +22,21 @@ const ProfilePage = (props) => {
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
-              className="w-[286px] h-[286px] rounded-full"
+              className="w-[186px] h-[186px] md:w-[286px] md:h-[286px] rounded-full"
             ></div>
             <h2 className="mt-[20px] font-bold">
               {ctx.user.firstName} {ctx.user.lastName}
             </h2>
             <p>@{ctx.user.username}</p>
           </div>
-          <div className="flex flex-col items-center">
-            <button className="font-medium">About</button>
-            <button className="font-medium">Timeline</button>
-          </div>
         </div>
-        <div className="w-[60%] flex justify-center mt-[100px]">
-          <div className="w-[70%]">
+        <div className="w-[95%] md:w-[60%] flex justify-center md:mt-[100px]">
+          <div className="w-[95%] md:w-[70%]">
             <h2 className="font-bold text-[20px] border-b-2 pb-[10px]">
               About
             </h2>
-            <div className="w-[100%] flex flex-wrap pt-[20px]">
-              <div className="w-[50%] h-[100px] py-[10px] text-[14px] ">
+            <div className="w-[100%] flex flex-wrap justify-between pt-[20px]">
+              <div className="h-[100px] py-[10px] text-[14px] mx-[10px]">
                 <p>
                   <span className="font-bold">Name:</span> {ctx.user.firstName}{" "}
                   {ctx.user.lastName}
@@ -53,7 +49,7 @@ const ProfilePage = (props) => {
                   <span className="font-bold">From:</span> {ctx.user.country}
                 </p>
               </div>
-              <div className="w-[50%] h-[100px] py-[10px] text-[14px]">
+              <div className="h-[100px] py-[10px] text-[14px] mx-[10px]">
                 <p>
                   <span className="font-bold">Age:</span> 24
                 </p>
@@ -62,19 +58,19 @@ const ProfilePage = (props) => {
                 </p>
                 <img src={Stars} width="100px" alt="star" />
               </div>
-              <div className="flex flex-col w-[50%] h-[100px] py-[10px] text-[14px]">
+              <div className="flex flex-col h-[100px] py-[10px] text-[14px] mx-[10px]">
                 <h2 className="text-[18px] font-bold">Languages</h2>
                 <p>&#x2022; English</p>
                 <p>&#x2022; French</p>
                 <p>&#x2022; Turkish</p>
               </div>
-              <div className="flex flex-col w-[50%] h-[100px] py-[10px] text-[14px]">
+              <div className="flex flex-col h-[100px] py-[10px] text-[14px] mx-[10px]">
                 <h2 className="text-[18px] font-bold">Currently in</h2>
                 <p>{ctx.user.country}</p>
               </div>
               <div className="w-[100%] mt-[30px]">
                 <h2 className="text-[16px] font-bold">Suggested places</h2>
-                <div className="flex">
+                <div className="flex justify-around">
                   <UserProfileIcons image={place1} name="Sarajevo" country="" />
                   <UserProfileIcons image={place2} name="Morocco" country="" />
                   <UserProfileIcons image={place3} name="Lisabon" country="" />
